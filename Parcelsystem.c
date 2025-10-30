11161432fecdf5449ba34c377a3e98536ca9afd2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-30 16:02:33 +0530

    Initialized parcel stack setup with dynamic memory allocation

--- Parcelsystem.c
+++ Parcelsystem.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void push(int stack[], int *top, int size, int parcelID);
+void pop(int stack[], int *top);
+void display(int stack[], int top);
+
+int main() {
+    int size;
+    printf("Enter number of parcels: ");
+    scanf("%d", &size);
+
+    int *stack = malloc(sizeof(int) * size);
+    if (stack == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
